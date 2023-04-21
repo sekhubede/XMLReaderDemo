4fe96fb8be6e13791a34022ac3bfd6e8bbfd2d45
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-21 09:40:09 +0200

    Added a Configuration class

---
+++ XmlLibrary/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlLibrary
+{
+    public class Configuration
+    {
+        public List<ImportInstruction> Instructions 
+            = new List<ImportInstruction>();
+    }
+}
