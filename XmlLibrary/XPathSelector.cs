523111e2a67f8c5ee7a93dacc9a8098167565f15
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-21 09:41:35 +0200

    Added an XPathSelector base class

---
+++ XmlLibrary/XPathSelector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlLibrary
+{
+    public class XPathSelector
+    {
+        public string XPathQuery { get; set; }
+    }
+}
