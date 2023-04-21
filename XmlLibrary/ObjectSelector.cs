09ddee7a5829fe9d1ec516b1667c183e4478a766
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-21 09:40:52 +0200

    Added an ObjectSelector class

---
+++ XmlLibrary/ObjectSelector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlLibrary
+{
+    public class ObjectSelector
+        : XPathSelector
+    {
+        public List<PropertySelector> PropertySelectors { get; set; } 
+            = new List<PropertySelector>();
+    }
+}
