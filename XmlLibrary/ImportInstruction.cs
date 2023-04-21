89781dfea6c96652eb84fe7b17291aa9996a5ef8
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-21 09:40:35 +0200

    Added an ImportInstruction class

---
+++ XmlLibrary/ImportInstruction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlLibrary
+{
+    public class ImportInstruction
+    {
+        public List<string> PathsToSearch 
+            = new List<string>();
+
+        public string SearchPattern { get; set; } 
+            = "*.xml";
+    }
+}
